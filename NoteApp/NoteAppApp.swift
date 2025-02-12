8f2bcf05b4555bfc3d51e02fb8cb6a457a71b88a
ILGIN DOGAN <ilgin.dogan@turkcell.com.tr>
2025-02-12 16:43:43 +0300

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by ILGIN DOGAN on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteAppTests/NoteAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppTests.swift
+//  NoteAppTests
+//
+//  Created by ILGIN DOGAN on 12.02.2025.
+//
+
+import Testing
+@testable import NoteApp
+
+struct NoteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
